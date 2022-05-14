3c8e5e6734ee457922b8d690c6918cdec75c59c3
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-05-14 23:38:54 +0100

    Header added. Project to be pushed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 // import Header from "./components/Header";
 import Home from "./components/Home";
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
+      <Header />
       <Home />
     </div>
   );
