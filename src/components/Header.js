eb9bd9c3d2a6e452438ad3c6d1281fb3f54a9c5d
Samuel Oyedeji <32018370+sammyteeoye@users.noreply.github.com>
2022-05-14 23:55:51 +0100

    Project completed

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,54 @@
-import React from 'react'
+import React, { useState } from 'react'
+import logo from "../images/logo.svg"
+import CloseButton from 'react-bootstrap/CloseButton'
+
 
 function Header() {
+  const [sideBarStatus, setSideBarStatus] = useState(false);
+  const sideBarStyle = {
+    transform: sideBarStatus ? "translateX(0)" : "translateX(100%)"
+  }
+  const menuStyle = {
+    backgroundColor: "#ccc",
+    padding: "0 5px",
+    borderRadius: "5px"
+  }
+
   return (
-    <div>Header</div>
+    <div className="header--container">
+      <a><img src={logo} alt="" /></a>
+      <div className="menu--items--left">
+        <a href="#">Model S</a>
+        <a href="#">Model 3</a>
+        <a href="#">Model X</a>
+        <a href="#">Model Y</a>
+        <a href="#">Solar Roof</a>
+        <a href="#">Solar Panel</a>
+      </div>
+      <div className="menu--items--right">
+        <a href="#">Shop</a>
+        <a href="#">Account</a>
+        <a href="#" onClick={() => setSideBarStatus(true)} style={menuStyle}>Menu</a>
+      </div>
+      <div className="side--menu" style={sideBarStyle}>
+        <div className="close--btn--container">
+          <CloseButton onClick={() => setSideBarStatus(false)} className="close--btn"/>
+        </div>
+        <li><a href="#">Model S</a></li>
+        <li><a href="#">Model 3</a></li>
+        <li><a href="#">Model X</a></li>
+        <li><a href="#">Model Y</a></li>
+        <li><a href="#">Solar Roof</a></li>
+        <li><a href="#">Solar Panels</a></li>
+        <li><a href="#">Existing Inventory</a></li>   
+        <li><a href="#">Used Inventory</a></li> 
+        <li><a href="#">Trade-In</a></li>
+        <li><a href="#">Test Drive</a></li>
+        <li><a href="#">Insurance</a></li>
+        <li><a href="#">Powerwall</a></li>
+        <li><a href="#">Commercial Energy</a></li>           
+      </div>
+    </div>
   )
 }
 
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import downarrow from "../images/down-arrow.svg"
+import Fade from 'react-reveal/Fade'
 
 function Section(props) {
 
@@ -9,17 +10,21 @@ function Section(props) {
 
   return (
     <div className="section" style={bgImage}>
+      <Fade bottom>
         <div className="section--text">
             <h1>{props.title}</h1>
             <p>{props.description} {props.underlineText && <u>Touchless Delivery.</u>}</p>
-        </div> 
+        </div>
+      </Fade>
+      <Fade bottom>
         <div className="button--group">
             <div className="section--btns">
                 <button>{props.leftBtnText}</button>
                 {props.rightBtnText && <button>{props.rightBtnText}</button>}
             </div> 
             <img src={downarrow} alt="down-arrow" className="down--arrow" />
-        </div>      
+        </div>
+      </Fade>
     </div>
   )
 }
